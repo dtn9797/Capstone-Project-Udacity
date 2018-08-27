a20cf01df36c429ba5b0591674e66e922bf54287
Duy Nguyen <dtn9797@gmail.com>
2018-08-26 22:57:25 -0500

    ..

--- app/src/main/java/com/example/duynguyen/amashop/MainActivity.java
+++ app/src/main/java/com/example/duynguyen/amashop/MainActivity.java
@@ -1,13 +1,164 @@
 package com.example.duynguyen.amashop;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.identity.intents.model.UserAddress;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.gms.wallet.AutoResolveHelper;
+import com.google.android.gms.wallet.CardInfo;
+import com.google.android.gms.wallet.CardRequirements;
+import com.google.android.gms.wallet.IsReadyToPayRequest;
+import com.google.android.gms.wallet.PaymentData;
+import com.google.android.gms.wallet.PaymentDataRequest;
+import com.google.android.gms.wallet.PaymentMethodTokenizationParameters;
+import com.google.android.gms.wallet.PaymentsClient;
+import com.google.android.gms.wallet.TransactionInfo;
+import com.google.android.gms.wallet.Wallet;
+import com.google.android.gms.wallet.WalletConstants;
+import com.stripe.android.model.Token;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
+
+    PaymentsClient mPaymentsClient;
+    final int LOAD_PAYMENT_DATA_REQUEST_CODE = 200;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPaymentsClient =
+                Wallet.getPaymentsClient(this,
+                        new Wallet.WalletOptions.Builder().setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                                .build());
+
+        findViewById(R.id.buy_button).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        PaymentDataRequest request = createPaymentDataRequest();
+                        if (request != null) {
+                            AutoResolveHelper.resolveTask(
+                                    mPaymentsClient.loadPaymentData(request),
+                                    MainActivity.this,
+                                    LOAD_PAYMENT_DATA_REQUEST_CODE);
+                            // LOAD_PAYMENT_DATA_REQUEST_CODE is a constant integer of your choice,
+                            // similar to what you would use in startActivityForResult
+                        }
+                    }
+                });
+
+
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case LOAD_PAYMENT_DATA_REQUEST_CODE:
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        PaymentData paymentData = PaymentData.getFromIntent(data);
+                        // You can get some data on the user's card, such as the brand and last 4 digits
+                        CardInfo info = paymentData.getCardInfo();
+                        // You can also pull the user address from the PaymentData object.
+                        UserAddress address = paymentData.getShippingAddress();
+                        // This is the raw JSON string version of your Stripe token.
+                        String rawToken = paymentData.getPaymentMethodToken().getToken();
+
+                        // Now that you have a Stripe token object, charge that by using the id
+                        Token stripeToken = Token.fromString(rawToken);
+                        if (stripeToken != null) {
+                            // This chargeToken function is a call to your own server, which should then connect
+                            // to Stripe's API to finish the charge.
+                            Toast.makeText(MainActivity.this,
+                                    "Got token " + stripeToken.toString(), Toast.LENGTH_LONG).show();
+                        }
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        break;
+                    case AutoResolveHelper.RESULT_ERROR:
+                        Status status = AutoResolveHelper.getStatusFromIntent(data);
+                        // Log the status for debugging
+                        // Generally there is no need to show an error to
+                        // the user as the Google Payment API will do that
+                        break;
+                    default:
+                        // Do nothing.
+                }
+                break; // Breaks the case LOAD_PAYMENT_DATA_REQUEST_CODE
+            // Handle any other startActivityForResult calls you may have made.
+            default:
+                // Do nothing.
+        }
+    }
+
+    private void isReadyToPay() {
+        IsReadyToPayRequest request = IsReadyToPayRequest.newBuilder()
+                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                .build();
+        Task<Boolean> task = mPaymentsClient.isReadyToPay(request);
+        task.addOnCompleteListener(
+                new OnCompleteListener<Boolean>() {
+                    public void onComplete(Task<Boolean> task) {
+                        try {
+                            boolean result =
+                                    task.getResult(ApiException.class);
+                            if(result == true) {
+                                //show Google as payment option
+                            } else {
+                                //hide Google as payment option
+                            }
+                        } catch (ApiException exception) { }
+                    }
+                });
+    }
+
+    private PaymentMethodTokenizationParameters createTokenizationParameters() {
+        return PaymentMethodTokenizationParameters.newBuilder()
+                .setPaymentMethodTokenizationType(WalletConstants.PAYMENT_METHOD_TOKENIZATION_TYPE_PAYMENT_GATEWAY)
+                .addParameter("gateway", "stripe")
+                .addParameter("stripe:publishableKey", "pk_test_TYooMQauvdEDq54NiTphI7jx")
+                .addParameter("stripe:version", "5.1.0")
+                .build();
+    }
+
+    private PaymentDataRequest createPaymentDataRequest() {
+        PaymentDataRequest.Builder request =
+                PaymentDataRequest.newBuilder()
+                        .setTransactionInfo(
+                                TransactionInfo.newBuilder()
+                                        .setTotalPriceStatus(WalletConstants.TOTAL_PRICE_STATUS_FINAL)
+                                        .setTotalPrice("10.00")
+                                        .setCurrencyCode("USD")
+                                        .build())
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_CARD)
+                        .addAllowedPaymentMethod(WalletConstants.PAYMENT_METHOD_TOKENIZED_CARD)
+                        .setCardRequirements(
+                                CardRequirements.newBuilder()
+                                        .addAllowedCardNetworks(Arrays.asList(
+                                                WalletConstants.CARD_NETWORK_AMEX,
+                                                WalletConstants.CARD_NETWORK_DISCOVER,
+                                                WalletConstants.CARD_NETWORK_VISA,
+                                                WalletConstants.CARD_NETWORK_MASTERCARD))
+                                        .build())
+                                        .setShippingAddressRequired(true);
+
+        request.setPaymentMethodTokenizationParameters(createTokenizationParameters());
+        return request.build();
+    }
+
+
+
 }
