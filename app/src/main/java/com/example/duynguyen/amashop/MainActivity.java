df3853b4818a275e25a4473893ea3c1ba235db82
Duy Nguyen <dtn9797@gmail.com>
2018-08-28 17:22:16 -0500

    Figured out

--- app/src/main/java/com/example/duynguyen/amashop/MainActivity.java
+++ app/src/main/java/com/example/duynguyen/amashop/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.duynguyen.amashop;
 
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageButton imageButton = findViewById(R.id.button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageButton.setSelected(!imageButton.isSelected());
+                imageButton.setImageDrawable(getDrawable(R.drawable.ic_check_black_24dp));
+//                imageButton.setImageDrawable(null);
+                imageButton.setBackgroundColor(getColor(R.color.colorAccent));
+
+            }
+        });
     }
 }
