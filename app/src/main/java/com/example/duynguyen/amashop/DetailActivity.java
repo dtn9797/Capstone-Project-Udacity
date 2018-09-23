5877c3306f6ad0da195f0a6b2a14c6a975631b13
Duy Nguyen <dtn9797@gmail.com>
2018-09-23 09:51:55 -0500

    Implementing Task 6: Fixed warning in detail xml/ bind views in detail activity

--- app/src/main/java/com/example/duynguyen/amashop/DetailActivity.java
+++ app/src/main/java/com/example/duynguyen/amashop/DetailActivity.java
@@ -1,16 +1,77 @@
 package com.example.duynguyen.amashop;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import com.ramotion.fluidslider.FluidSlider;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class DetailActivity extends AppCompatActivity {
+
+    public final static String TAG = DetailActivity.class.getSimpleName();
+    public final static String PRODUCT_EXTRA = "product";
+
+    @BindView(R.id.app_bar)
+    Toolbar toolbar;
+    @BindView(R.id.product_iv)
+    ImageView productIv;
+    @BindView(R.id.product_title_tv)
+    TextView titleTv;
+    @BindView(R.id.price_tv)
+    TextView priceTv;
+    @BindView(R.id.rating_bar)
+    RatingBar ratingBar;
+    @BindView(R.id.description_tv)
+    TextView descriptionTv;
+    @BindView(R.id.colors_ll)
+    LinearLayout colorsLl;
+    @BindView(R.id.fluidSlider)
+    FluidSlider fluidSlider;
+    @BindView(R.id.add_cart_button)
+    Button addButton;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+        ButterKnife.bind(this);
+
+        setUpToolbar();
+
+
+    }
+
+    private void setUpToolbar() {
+        setSupportActionBar(toolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.add_product_button) {
+            Log.d(TAG, "Add product button is clicked");
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
