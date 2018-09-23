2405d98f9d250118c2c213ca5946e7eb39ee8804
Duy Nguyen <dtn9797@gmail.com>
2018-09-23 08:33:58 -0500

    Implementing - Task6: Middle of creating detail activity layout

---
+++ app/src/main/java/com/example/duynguyen/amashop/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.example.duynguyen.amashop;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+}
