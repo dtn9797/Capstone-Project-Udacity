9abae8761149a3a1f8833be25e8cefd15ba6fb60
Duy Nguyen <dtn9797@gmail.com>
2018-10-09 23:44:52 -0500

    Implementing Task 10: initilizing widget xml, java, manifest

--- app/src/main/java/com/example/duynguyen/amashop/widgets/NewAppWidget.java
+++ app/src/main/java/com/example/duynguyen/amashop/widgets/NewAppWidget.java
@@ -1,9 +1,14 @@
-package com.example.duynguyen.amashop;
+package com.example.duynguyen.amashop.widgets;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
+
+import com.example.duynguyen.amashop.MainActivity;
+import com.example.duynguyen.amashop.R;
 
 /**
  * Implementation of App Widget functionality.
@@ -16,7 +21,15 @@ public class NewAppWidget extends AppWidgetProvider {
         CharSequence widgetText = context.getString(R.string.appwidget_text);
         // Construct the RemoteViews object
         RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.new_app_widget);
-        views.setTextViewText(R.id.appwidget_text, widgetText);
+        views.setTextViewText(R.id.brand_name, widgetText);
+
+        //Create an intent
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+        views.setOnClickPendingIntent(R.id.brand_name,pendingIntent);
+
+        //empty view
+        views.setEmptyView(R.id.products_list,R.id.empty_view);
 
         // Instruct the widget manager to update the widget
         appWidgetManager.updateAppWidget(appWidgetId, views);
@@ -25,11 +38,14 @@ public class NewAppWidget extends AppWidgetProvider {
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
+        updateAppWidgets(context,appWidgetManager,appWidgetIds);
+    }
+
+    public static void updateAppWidgets (Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
         }
     }
-
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
